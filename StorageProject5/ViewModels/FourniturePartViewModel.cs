64e57b6811d5f31b7134ff16cf99f087c4679e54
admin <torrentdu64@gmail.com>
2020-06-28 12:22:14 +1200

    add Furniture Part View Model object

---
+++ StorageProject5/ViewModels/FourniturePartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StorageProject5.Models;
+
+namespace StorageProject5.ViewModels
+{
+    public class FourniturePartViewModel
+    {
+        public  Furniture Furniture { get; set; }
+        public Part Part { get; set; }
+    }
+}

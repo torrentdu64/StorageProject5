9f95a70c1dbc5d02b220db75050bf3e79602b16d
admin <torrentdu64@gmail.com>
2020-07-04 07:16:16 +1200

    rental side start

---
+++ StorageProject5/Controllers/RentalsController.cs
@@ -0,0 +1,29 @@
+﻿using StorageProject5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorageProject5.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        // GET: Rentals
+        public ActionResult Index()
+        {
+            var furnitures = _context.Furnitures.ToList();
+
+            return View(furnitures);
+        }
+    }
+}
